5d2d022170a2eada4c3c544f4328b09b6c334b9b
colas98 <colas98@gmail.com>
2023-01-09 14:55:05 +0100

    class 11 and 12

--- ablog/theblog/forms.py
+++ ablog/theblog/forms.py
@@ -1,15 +1,22 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
 
+# choices =  [('coding', 'coding'), ('essais', 'essais'), ('contes', 'contes'),]
+choices = Category.objects.all().values_list('name','name')
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body', 'first_post_date')
+        fields = ('title', 'title_tag', 'author', 'category', 'body', 'first_post_date')
 
         widgets = {
             'title': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'ex: This is Title Placeholder stuff'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices = choice_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             'first_post_date': forms.DateInput(attrs={'class': 'form-control', 'placeholder' : 'ex: 2022-01-30'})
         }
