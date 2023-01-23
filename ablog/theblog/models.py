48757d1175b4ff580dfdd1462a19b39519d2597d
colas98 <colas98@gmail.com>
2023-01-23 13:59:19 +0100

    class 18, 19

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -20,6 +20,11 @@ class Post(models.Model):
     post_date = models.DateField(auto_now_add=True)
     first_post_date = models.DateField()
     category = models.CharField(max_length= 255, default='uncategorized')
+    likes = models.ManyToManyField(User, related_name = 'blog_posts')
+
+    def total_likes(self):
+        return self.likes.count()
+
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
